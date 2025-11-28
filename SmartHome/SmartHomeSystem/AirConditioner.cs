fa8f3212c08f6340af2890ab472d30bf87953064
Bud130707 <olonetsdev@gmail.com>
2025-11-29 01:04:31 +0200

    lab4

---
+++ SmartHome/SmartHomeSystem/AirConditioner.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SmartHomeSystem
+{
+    public class AirConditioner : Device, IEnergyConsumer
+    {
+        public const int POWER = 2000;
+
+        public AirConditioner(string name) : base(name) { }
+
+        public string DeviceName => Name;
+        public int PowerConsumption => POWER;
+
+        public override void TurnOn()
+        {
+            IsOn = true;
+            Console.WriteLine($"{Name} почав охолодження");
+        }
+
+        public override void TurnOff()
+        {
+            IsOn = false;
+            Console.WriteLine($"{Name} зупинено");
+        }
+
+        public double GetEnergyUsage(int hours)
+        {
+            if (!IsOn)
+            {
+                return 0.0;
+            }
+
+            return PowerConsumption * hours / 1000.0;
+        }
+    }
+}
---
+++ SmartHome/SmartHomeSystem/CoffeeMachine.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SmartHomeSystem
+{
+    public class CoffeeMachine : Device, IEnergyConsumer
+    {
+        public const int POWER = 1000;
+
+        public CoffeeMachine(string name) : base(name) { }
+
+        public string DeviceName => Name;
+        public int PowerConsumption => POWER;
+
+        public override void TurnOn()
+        {
+            IsOn = true;
+            Console.Write($"{Name} почала готувати каву.");
+        }
+
+        public override void TurnOff()
+        {
+            IsOn = false;
+            Console.Write($"{Name} завершила роботу.");
+        }
+
+        public double GetEnergyUsage(int hours)
+        {
+            if (!IsOn)
+            {
+                return 0.0;
+            }
+
+            return PowerConsumption * hours / 1000.0;
+        }
+    }
+}
