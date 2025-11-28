33c7b5b1ebb2d200449caa4660635a2984f06c28
Bud130707 <olonetsdev@gmail.com>
2025-11-29 01:01:20 +0200

    lab3

---
+++ DeliverySystem/DeliverySystem/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+namespace DeliverySystem
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Vehicle scooter = new Scooter("Honda", 2018, 1200, 30);
+            Vehicle car = new Car("BMW", 2011, 34000, 4);
+            Vehicle van = new Van("Ford", 2020, 56000, 5, 1000);
+
+            Console.WriteLine(scooter.GetInfo());
+            Console.WriteLine($"Max speed: {scooter.GetMaxSpeed()} km/h");
+            scooter.Move(20);
+            ((Scooter)scooter).Charge();
+
+            Console.WriteLine(car.GetInfo());
+            Console.WriteLine($"Max speed: {car.GetMaxSpeed()} km/h");
+            car.Move(50);
+
+            Console.WriteLine(van.GetInfo());
+            Console.WriteLine($"Max speed: {van.GetMaxSpeed()} km/h");
+            ((Van)van).LoadCargo(800);
+            Console.WriteLine(van.GetInfo());
+            ((Van)van).LoadCargo(300);
+            ((Van)van).UnloadCargo();
+        }
+    }
+}
